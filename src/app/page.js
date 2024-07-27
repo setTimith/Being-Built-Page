ec0c7daed7d9aab642a42fb3412c078d68a36744
setTimith <thomasabboud0@gmail.com>
2024-07-27 19:46:12 +0300

    First Commit

---
+++ src/app/page.js
@@ -0,0 +1,21 @@
+import "./page.css";
+import bg from "../../public/julien-moreau-688Fna1pwOQ-unsplash.jpg";
+
+export default function Home() {
+  return (
+    <div
+      className="mainContainer"
+      style={{
+        backgroundImage: `url(${bg.src})`,
+      }}
+    >
+      <div className="shader"></div>
+      <div className="contentDiv">
+        <h1>Company Title </h1>
+        <p>Currently being Built out</p>
+        <p>Currently being Built out</p>
+        <p>Currently being Built out</p>
+      </div>
+    </div>
+  );
+}
